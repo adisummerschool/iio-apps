5d59dc1b734c72284ff4bb24a3d8fc8c6ad2f83b
Maxim Bianca <bmaxim55@yahoo.com>
2022-08-24 20:59:44 +0300

    Added buffer functions (create, read and decode)

--- getbuffer.c
+++ getbuffer.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iio.h>
+#include <byteswap.h>
 
 #define URI "ip:10.76.84.212"
 #define DEVICE_NAME "ad5592r"
@@ -7,6 +8,18 @@
 struct iio_context *ctx = NULL;
 struct iio_device *dev = NULL;
 struct iio_channel *ch[4];
+struct iio_buffer *buf = NULL;
+
+#define THRESH 40
+
+unsigned createMask(unsigned a, unsigned b)
+{
+    unsigned r = 0;
+    for (unsigned i=a; i<=b; i++)
+        r |= 1 << i;
+
+    return r;
+}
 
 int main() {
 
@@ -23,7 +36,6 @@ int main() {
     }
     printf("Got device %s %lx\n", DEVICE_NAME, dev);
 
-
     int nr_chan;
     nr_chan = iio_device_get_channels_count(dev);
     printf("IIO device has %u channels:\n", nr_chan);
@@ -35,25 +47,62 @@ int main() {
             printf("Channel not found\n");
             return -1;
         }
+        iio_channel_enable(ch[i]);
+
         printf("Got channel %s %lx\n", iio_channel_get_id(ch[i]), ch[i]);
     }
 
-        int values[4];
+    buf = iio_device_create_buffer(dev, 100, false);
+    if(buf == NULL) {
+        printf("Could not create buffer\n");
+        return -1;
+    }
 
-        while(1) {
-             for (int i=0; i < 4; i++) {
-                        char buf[10];
-                        iio_channel_attr_read(ch[i], "raw", &buf, 10);
-                        values[i] = atoi(buf);
-             }
+        int nr_bytes;
+        nr_bytes = iio_buffer_refill(buf);
+        if(nr_bytes < 0)
+            printf("Error refilling buf %d\n", nr_bytes);
+        else printf("The nr of bytes is %d\n", nr_bytes);
 
-            int x = values[0] - values[1];
-            int y = values[2] - values[3];
+        int x,y,prevx,prevy;
+            bool first=true;
+            while(1) {
+            for (char *ptr = iio_buffer_start(buf);
+                 ptr < iio_buffer_end(buf);
+                 ptr += iio_buffer_step(buf)) {
 
-            printf("x = %5d\ty = %5d\n", x,y);
-            fflush(stdout);
-            usleep(500 * 1000);
-      }
+                int16_t values[4];
+                int16_t* ptrs[4];
+
+                ptrs[0] = (int16_t*)(ptr);
+                ptrs[1] = (int16_t*)(ptr+2);
+                ptrs[2] = (int16_t*)(ptr+4);
+                ptrs[3] = (int16_t*)(ptr+6);
+
+                values[0] = *ptrs[0];
+                values[1] = *ptrs[1];
+                values[2] = *ptrs[2];
+                values[3] = *ptrs[3];
+
+                prevx = x;
+                prevy = y;
+                x = values[0] - values[1];
+                y = values[2] - values[3];
+
+        //		printf("%d %d %d %d\n" ,prevx,prevy,x,y );
+                if(first) {
+                    first = false;
+                    continue;
+                }
+                if(abs(x-prevx) > THRESH || abs(y-prevy)>THRESH) {
+
+                    printf("HERE");
+                }
+
+            }
+            }
+
+        iio_buffer_destroy(buf);
 
     return 0;
 }
