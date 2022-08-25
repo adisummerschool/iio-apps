6cc363aaead5fbbb2c61105dea8f7a8418c933bd
rusurazvan123 <rusurazvan779@gmail.com>
2022-08-25 11:39:56 +0300

    Fixed buffers

--- buffer.c
+++ buffer.c
@@ -86,6 +86,7 @@ while(1) {
         struct tm tm = *localtime(&t);
         printf("now: %d-%02d-%02d %02d:%02d:%02d: ", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
         printf("Accelerometer Event: X:%5d Y:%5d\n", eventX, eventY);
+        fflush(stdout);
     }
 }
 
